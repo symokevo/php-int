fd82a7c1756a42a59f57ba0fda724b59c165e581
Simon Rabuogi <symokevo@gmail.com>
2023-07-23 08:46:28 +0300

    create name.php to collect name variables from the html form and display them in the page

---
+++ name.php
@@ -0,0 +1,8 @@
+<?php
+    $firstName = $_GET['firstname'];
+    $lastName = $_GET['lastname'];
+
+    echo 'Welcome to our website, ' . 
+    htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' . 
+    htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
+?>
