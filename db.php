4b7f79e837a8ff34962fb4d457a288658130e823
Simon Rabuogi <symokevo@gmail.com>
2023-07-27 05:50:10 +0300

    Connect the db from php scripts and query the db to alter it from php

--- db.php
+++ db.php
@@ -1,10 +1,19 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql: host=localhost;dbname=zimadb', 'zimaster', 'zimaster');
-    $output = 'Database Connection established.';
+    $pdo = new PDO('mysql: host=localhost;dbname=zimadb;charset=utf8mb4', 'zimaster', 'zimaster');
+
+    // query the db
+    $sql = 'UPDATE joke SET jokedate="2023-7-10" WHERE joketext LIKE "%programmer%"';
+
+    $affectedRows = $pdo->exec($sql);
+
+    $output = 'Updated ' . $affectedRows . ' rows.';
 } catch (PDOException $e) {
-    $output = 'Unable to connect ot the database server: ' . $e->getMessage();
+    $output = 'Database Error: ' . $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine();
+    // notice the use of -> to access the properties of an object in php
+    // a function stored in an object is called a method
+    // Disconnect the server by setting: $pdo = null;
 }
 
 include __DIR__ . '/templates/output.html.php';
